613c5726cd14d17e1b9558ae15a9d44aa405efd4
Goynang <sid@latterfamily.com>
2010-02-07 00:29:26 +0000

    Random batch of improvements. Better documentation. Refactorings. Support for local testing via yaml files.

--- plugins/are_you.rb
+++ plugins/are_you.rb
@@ -1,10 +1,10 @@
-Plugin.define("are you (.*?)\?") do
+Plugin.define("are you ([a-zA-Z0-9 ]*)\?") do
   name    "Are you?"
   version "1.0.0"
   details "Find out if you are a something."
   usage   "Are you a xxx?"
   
-  def run(client, tweet, arguments)
-    client.update "@#{tweet.from_user} No I'm not #{arguments.first}!"
+  def run(tweet, matches)
+    "@#{tweet.from_user} No I'm not #{matches.first}!"
   end
 end
--- sage.rb
+++ sage.rb
@@ -1,44 +1,128 @@
 #!/usr/bin/env ruby
 
+# == Synopsis 
+#   Simple little Bot for Twitter.
+#   Uses plugins to extend the bot's capabilities.
+# 
+# == Details 
+#   When run in the normal manner it will connect to twitter and collect
+#   recent tweets sent to the account detailed within ./account.yml. It will
+#   then process these tweets using the currently available plugins.
+#
+#   Plugins sit within the 'plugins' directory and provide specific bits
+#   of functionality for the bot. Theses are effectively the commands that
+#   the bot can respond to.
+#
+#   To use the bot for real you must create an 'account.yml' file containing
+#   your twitter username and password. OAuth support will come in time.
+#
+#   If you want to test the bot out without actually tweeting anything out
+#   to the great unwashed public then you can run it in test mode. In this
+#   mode it will utilise a local yml file to provide incoming tweets and will
+#   simply return text indicating how it would have tweeted had it been
+#   running for real. This makes it easy to test out new plugins by crafting
+#   sample tweets and checking that the bot responds in the correct manner.
+#
+# == Usage
+#   To run the bot and process incoming messages etc. simply set up your
+#   account.yml file and then...
+#     sage.rb
+#
+#   To run it locally for testing purposes then do this...
+#     sage.rb --test
+#
+#   If you run locally then be sure to create some yml files to use in place
+#   of the real twitter API. See example files for more details.
+#
+# == Options
+#   -h, --help          Displays help message
+#   -v, --version       Display the version, then exit
+#   -V, --verbose       Verbose output
+#   -t, --test          Run in test mode making no remote connections
+
 require 'rubygems'
 require 'yaml'
+require 'hashie'
 require 'twitter'
+require 'optparse' 
+require 'rdoc/usage'
+require 'ostruct'
 require 'plugin.rb'
 
 class Sage
-  VERSION = '0.0.1'
+  VERSION = '0.0.2'
   
-  def initialize
+  attr_reader :options
+  
+  def initialize(arguments)
+    @arguments = arguments
+    
+    # Set defaults
+    @options = OpenStruct.new
+    @options.verbose = false
+    @options.testing = false
+    
     load_plugins  
   end
   
   def run
-    account = load_account
-    client  = create_client(account)
-    process_incoming_messages(account, client)
+    if parsed_options?
+      client = create_client
+      process_mentions(client)
+    end
   end
   
 protected
   
   def load_plugins
+    puts "Lodaing plugins from ./plugins directory" if @options.verbose
     Dir["plugins/*.rb"].each{ |x| load x }
   end
   
-  def load_account
-    account  = YAML::load(File.open('account.yml')) 
+  def parsed_options?
+    # Specify options
+    opts = OptionParser.new 
+    opts.on('-v', '--version')    { output_version ; exit 0 }
+    opts.on('-h', '--help')       { output_help }
+    opts.on('-V', '--verbose')    { @options.verbose = true }  
+    opts.on('-t', '--test')       { @options.testing = true }
+
+    opts.parse!(@arguments) rescue return false
+    true      
   end
   
-  def create_client(account)
-    httpauth = Twitter::HTTPAuth.new(account['username'], account['password'])
-    Twitter::Base.new(httpauth)
+  def output_version
+    puts "#{File.basename(__FILE__)} version #{VERSION}"
+  end
+  
+  def output_help
+    output_version
+    RDoc::usage() #exits app
+  end
+  
+  def create_client
+    if @options.testing
+      puts "Creating a fake client for local testing" if @options.verbose
+      LocalClient.new
+    else
+      puts "Attempting to login to twitter and create a client" if @options.verbose
+      account  = YAML::load(File.open('account.yml')) 
+      httpauth = Twitter::HTTPAuth.new(account['username'], account['password'])
+      Twitter::Base.new(httpauth)
+    end
   end
 
-  def process_incoming_messages(account, client)
-    Twitter::Search.new.to(account['username']).each do |tweet|
+  def process_mentions(client)
+    puts "Processing mentions (incoming messages)" if @options.verbose
+    # For each mention see if any plugins can respond to it.
+    # This nested loop is as ugly as hell and could be done better!
+    client.mentions.each do |tweet|
       Plugin.registered_plugins.each do |pattern, plugin|
-        regexp = Regexp.new("^@#{account['username']} #{pattern}$", Regexp::IGNORECASE)
-        if !(match = tweet.text.scan(regexp)).empty?
-          plugin.run(client, tweet, match)
+        regexp = Regexp.new(pattern, Regexp::IGNORECASE)
+        matches = tweet.text.scan(regexp)
+        puts " #{plugin.name} ==> #{tweet.text} ==> #{matches.inspect}" if @options.verbose
+        unless matches.empty?
+          client.update plugin.run(tweet, matches)
         end
       end
     end
@@ -46,5 +130,32 @@ protected
 
 end
 
-sage = Sage.new()
+class LocalClient
+  def initialize
+  end
+  
+  def update(text)
+    puts " ...#{text}"
+  end
+  
+  def mentions
+    mash_yaml('mentions.yml')
+  end
+  
+protected
+
+  # Takes a filename for a yaml file and converts its contents into a Mash.
+  # This is the object type that the twitter API uses internally so this lets
+  # us fake out API responses. Mashes are just Hashes with bells on!
+  def mash_yaml(filename)
+    things = []
+    YAML::load(File.open(filename)).each_value do |thing|
+      things << Hashie::Mash.new(thing)
+    end
+    things
+  end
+
+end
+
+sage = Sage.new(ARGV)
 sage.run
