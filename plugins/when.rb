eb27860dbaa955fceddd829c0d8980deeea542e0
Goynang <sid@latterfamily.com>
2010-02-07 08:29:26 +0800

    Random batch of improvements. Better documentation. Refactorings. Support for local testing via yaml files.

--- plugins/when.rb
+++ plugins/when.rb
@@ -1,12 +1,12 @@
 require 'chronic'
 
-Plugin.define("when is (.*?)\?") do
+Plugin.define("when is ([a-zA-Z0-9 ]*)\?") do
   name    "When is?"
   version "1.0.0"
   details "Find out when something is."
   usage   "When is xxx?"
   
-  def run(client, tweet, arguments)
-    client.update "@#{tweet.from_user} #{arguments.first} is #{Chronic.parse(arguments.first)}."
+  def run(tweet, matches)
+    "@#{tweet.from_user} #{matches.first} is #{Chronic.parse(matches.first)}."
   end
 end
